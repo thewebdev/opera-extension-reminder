12b28cccb9ca3c5b86dada50ae04de442051fb79
thewebdev <thewebdev@myopera.com>
2012-06-18 05:00:35 +0530

    start

---
+++ source/js/background.js
@@ -0,0 +1,262 @@
+﻿/* slide time delay */
+var slider; 
+/* background colors */
+var bg = ["#B80000", "#335300", "#003C5B", "#632561", "#72471F", 
+          "#7E5949", "#547D48", "#4F487D", "#487A7D", "#7D4875"];
+
+function $(v) {
+	/* DOM: identifies element */
+	if (document.getElementById(v)) {
+		return document.getElementById(v);
+	}
+}
+
+function E(v) {
+	/* DOM: creates new element */
+	return document.createElement(v);
+}
+
+function Txt(v) {
+	/* DOM: creates text nodes */
+	return document.createTextNode(v); 
+}
+
+function hide(id) {
+	$(id).style.display = 'none';
+}
+
+function show(id) {
+	$(id).style.display = 'block';
+}
+
+function createUl(kids) {
+/*  Creates the unordered list used to
+	display the data in the speed dial.
+	The 'kids' parameter specifies how
+	many list nodes  to create.	Once 
+	the unordered list is created,
+	the function only adds or deletes
+	list node as necessary. Opera 
+	recommends using createDocumentFragment()
+	as it is faster to create the elements
+	separately and then add to the page. */
+	
+	var ul, li, txt, temp;
+	var inHtml = document.createDocumentFragment();
+	var list = $("noteSlides");
+	
+	if (list) {
+		/*  if ul node exists */
+		
+		temp = list.getElementsByTagName('li');
+		
+		if (temp.length === kids) {
+			return;
+		} else if (temp.length < kids) {
+			/*  add more li nodes */
+			
+			var z = kids - temp.length;
+			
+			for (var a = 0; a < z; a++) {
+				li = E('li');
+				txt = Txt('');
+				li.appendChild(txt);
+				inHtml.appendChild(li);				
+			}
+			
+			list.appendChild(inHtml);
+			return;
+			
+		} else if (temp.length > kids) {
+			/*  delete some li nodes */
+			
+			var x = temp.length - kids;
+			
+			while (x !== 0) {
+				list.removeChild(temp[0]);
+				x -= 1;
+			}	
+			return;
+		}
+	} 
+	
+	/*  create the list and add to the DOM */
+		
+	ul = E('ul');
+	ul.setAttribute('id', 'noteSlides');
+	
+	for (var i = 0; i < kids; i++) {
+		li = E('li');
+		txt = Txt('');
+		li.appendChild(txt);
+		ul.appendChild(li);
+	}
+
+	inHtml.appendChild(ul);
+	$('data').appendChild(inHtml);
+	
+	return;
+}
+
+function prepData() {
+/*  Retrieves the saved notes and 
+    prepares it for display */
+	
+	var i, x;
+	var out = [];		
+	
+	refDial("wait");
+	
+	if (localStorage) {
+		if (localStorage.length) {
+			x = localStorage.length; 
+			for (i = 0; i < x; i++) {
+				key = localStorage.key(i);
+				note = String(localStorage.getItem(key));				
+				out[i] = note;
+			}
+		} else {
+			/* load some default data */
+			out[0] = "Smile";
+			out[1] = "Stop browsing. Start working."
+			out[2] = "Right-click. Select Preferences. Add notes."
+		}
+		
+		/*  send the data for o/p */
+		refDial("show", out);
+	} else {
+		refDial("hang");
+	}
+}
+
+function refDial(cmd, out) {
+	/* Show the updates and notes
+	   in the speed dial. */
+	
+	if (cmd == "show") {
+		var li, m, o;
+		
+		clearInterval(slider);	
+		
+		/* create the unordered list
+		   structure used to show the data. */
+		createUl(out.length);
+		
+		li = $("noteSlides").getElementsByTagName('li');
+		m = out.length;
+		
+		for (o = 0; o < m; o++) {
+			/*  add data */
+			if (li[o]) {
+				/*  reset css class */
+				li[o].className = "";
+				/*  assign the new data */
+				li[o].innerHTML = out[o];
+			} 		
+		}
+		
+		li[0].className = 'current';
+		
+		hide("wait");
+		show("data");
+		
+		/* each note is shown for a few seconds */
+		slider = setInterval(startSlide, parseInt(widget.preferences.showfor,10) * 1000);
+		
+		/*  start displaying the data */
+		startSlide();
+		return;
+	}
+	
+	if (cmd == "wait") {
+		/* used to indicate that an
+		   update of data is underway */
+		
+		$("msg").firstChild.nodeValue = "loading";
+
+		clearInterval(slider);			
+		hide("data");
+		show("wait");
+		
+		return;
+	}
+	
+	if (cmd == "hang") {
+		/* indicate some error
+		   has occured */
+		
+		$("msg").firstChild.nodeValue = "Something went wrong. Will retry after some time.";
+		
+		clearInterval(slider);	
+		hide("data");
+		show("wait");		
+		
+		return;
+	} 
+}
+
+function startSlide() {
+	/* Displays the notes.
+	   Cycles through each list node
+	   and marks the node to be displayed 
+	   with css class name 'current'. */
+	
+	var cls, li, done, tempLi, m, s;
+	
+	done = false;
+	tempLi = [];
+
+	li = $("noteSlides").getElementsByTagName('li');
+	m = li.length;
+	
+	for (var e=0; e < m; e++) {
+		/* Opera recommends making changes 
+		   to a copy of the DOM */
+		tempLi[tempLi.length] = li[e];
+	}
+	
+	s = tempLi.length;
+	
+	for (var i = 0; i < s; i++) {
+		if (done) { 
+			/* Once a list element has been marked
+			   'current', no need to go through
+			   the rest of it as we display only
+			   one list element at a time. */
+			
+			continue; 
+		}
+		
+		cls = tempLi[i].className;
+		
+		if ((cls.indexOf("current")) != -1) {
+			
+			/*  unmark the currently displayed li */
+			tempLi[i].className = "";
+			
+			if (i == (tempLi.length-1)) {
+				/* if we have reached the last 
+				   li, mark the first li again. */
+			
+				tempLi[0].className = 'current';
+			} else {
+				tempLi[i+1].className = 'current';
+			}
+			
+			done = true;
+			if (i > bg.length) {
+				document.body.style.backgroundColor = bg[0];			
+			} else {
+				document.body.style.backgroundColor = bg[i];			
+			}
+		}
+	}
+}
+
+function init() {
+	/* start */
+	prepData();
+}
+
+/*  monitor and inform when HTML file is ready */
+document.addEventListener('DOMContentLoaded', init, false);
---
+++ source/js/options.js
@@ -0,0 +1,257 @@
+﻿var stack = {};
+var count;
+
+function $(v) {
+	if (document.getElementById(v)) {
+		return document.getElementById(v);
+	}
+}
+
+function hide(id) {
+	$(id).style.display = 'none';
+}
+
+function show(id) {
+	$(id).style.display = 'block';
+}
+
+function E(v) {
+	return document.createElement(v);
+}
+
+function Txt(v) {
+	return document.createTextNode(v);
+}	
+
+function status(msg) {
+	/* Used to display messages
+	   to the user */
+	   
+	var hangTimer;
+	
+	$("msg").firstChild.nodeValue = msg;
+	show("msg");
+	
+	/* show status update for 7 seconds */
+	clearTimeout(hangTimer);
+	hangTimer = setTimeout(function() {
+		hide("msg");
+	}, 7000);	
+}
+
+function unlock() {
+	/* Enable save button but not if
+	   there is no notes to be saved 
+	   (indicated by count). */
+	   
+	if (count === 0) {
+		$('apply').disabled = true;
+	} else {
+		$('apply').disabled = false;
+	}
+}
+
+function genId() {
+	/* 
+	   original source - 
+	   http://www.mediacollege.com/internet/javascript/number/random.html
+	*/
+	var chars = "abcdefghiklmnopqrstuvwxyz";
+	var idSize = 8;
+	var id = '';
+	
+	for (var i = 0; i < idSize; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		id += chars.substring(rnum,rnum+1);
+	}
+	
+	return id;
+}
+
+function apply() {
+	/* Saves the changes permanently */
+	   
+	var cmd, note;
+	var ul, li;
+	var save = false;
+	
+	for (var id in stack) {
+		if (stack[id][0] == 'add'){
+			note = stack[id][1];
+			if (localStorage) {
+				localStorage.setItem(id, note)
+				save = true;
+			} else {
+				status("Error: Unable to save note.");
+			}
+		}
+		
+		if (stack[id][0] == 'remove') {
+			if (localStorage.getItem(id)) {
+				localStorage.removeItem(id)
+			}
+			save = true;
+		}
+	}
+	
+	/* reset stack */
+	stack = {};
+	
+	hide("set");
+	ul = $("set");
+	li = ul.getElementsByTagName("li");
+	
+	/* remove class name to flag that
+	   new notes have been saved */
+	   
+	for (var e = 0; e < li.length; e++) {
+		li[e].className = "";
+	}
+	
+	show("set");
+	
+	if (save) {
+		widget.preferences.count = count;
+		/* reload dial with new settings */		
+		opera.extension.bgProcess.prepData();		
+	}
+	
+	$('apply').disabled = true;	
+	return;
+}
+
+function removeNote() {
+	/* Allows user to delete a note. Note:
+	   The notes are not deleted immediately */
+	   
+	var id, li;
+	var temp;
+	
+	id = this.parentNode.id;
+	
+	li = $(id);
+
+	/* makes sure we don't delete a note
+	   that doesn't exist in localStorage */
+	
+	if (li.className == 'new') {
+		/* delete from stack only */
+		delete stack[id];
+	} else {
+		/* mark for deletion */
+		stack[id] = ['remove'];
+	}
+	
+	count = count - 1;
+
+	/* Validation: there should be atleast one
+       note for the extension to function */
+	   
+	if (count == 0) {
+		$('apply').disabled = true;
+	} else {
+		$('apply').disabled = false;
+	}
+	
+	li.parentNode.removeChild(li);
+}
+
+function addNote() {
+	/* Allows the user to add new 
+	   notes and displays it. */
+	   
+	var key, note;
+	var li, id, a, txt;
+	
+	note = document.input.remind.value;
+	
+	id = genId();
+	
+	li = E("li");
+	li.setAttribute('id', id);
+	li.className = "new";
+	
+	a = E("a");
+	a.setAttribute('href', '#self');
+	txt = Txt('delete');
+	a.appendChild(txt);
+	a.addEventListener('click', removeNote, false);
+	
+	txt = Txt(note);
+	
+	li.appendChild(a);
+	li.appendChild(txt);
+	
+	stack[id] = ["add", note];
+	
+	count += 1;
+		
+	$("set").appendChild(li);
+	
+	/* Enable the save button as
+	   user made changes. */
+	$('apply').disabled = false;
+	
+	return;
+}
+
+function load() {
+	/* Loads the saved notes and displays 
+	   it to the user for making changes. */ 
+	   
+	var key, note;
+	
+	var ul, li, a, txt;
+	var inHtml = document.createDocumentFragment();
+
+	hide("set");
+	
+	/* Creates a ul list to display 
+	   the saved notes */
+	   
+	if (localStorage) {
+		if (localStorage.length) {
+			x = localStorage.length; 
+			for (i = 0; i < x; i++) {
+				key = localStorage.key(i);
+				note = String(localStorage.getItem(key));
+				
+				li = E("li");
+				li.setAttribute('id', key);
+				
+				a = E("a");
+				a.setAttribute('href', '#self' + i);
+				txt = Txt('delete');
+				a.appendChild(txt);
+				a.addEventListener('click', removeNote, false);
+
+				txt = Txt(note);				
+				li.appendChild(a);
+				li.appendChild(txt);
+				
+				inHtml.appendChild(li);
+			}
+			
+		$("set").appendChild(inHtml);			
+		}	   
+	}
+	show("set");
+}
+
+function init() {
+	/* some basic settings intialised here */
+	
+	count = widget.preferences.count;
+	
+	/* disable save button on start */
+	$('apply').disabled = true;
+	
+	/* monitor for button clicks */
+	$('add').addEventListener('click', addNote, false); 
+	$('apply').addEventListener('click', apply, false);
+	
+	load();
+}
+
+/*  monitor and inform when HTML file is ready */
+document.addEventListener('DOMContentLoaded', init, false);
