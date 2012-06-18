67e1c7976bf78cfbc8bfbbb1b5fedab61078de4b
thewebdev <thewebdev@myopera.com>
2012-06-18 07:20:14 +0530

    added license file and notice

--- source/js/options.js
+++ source/js/options.js
@@ -1,4 +1,28 @@
-﻿var stack = {};
+﻿/*  This file is part of The Reminder. The Reminder	is an 
+	Opera extension that Displays notes in your Opera Speed 
+	Dial to help you remember it.
+	
+    Copyright (C) 2012 M Shabeer Ali
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	Website: http://getreminder.tumblr.com/
+	Source code: https://github.com/thewebdev/opera-extension-reminder.git 
+	Email: thewebdev@myopera.com 
+*/
+
+var stack = {};
 var count;
 
 function $(v) {
